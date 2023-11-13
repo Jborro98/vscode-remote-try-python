b7886c77734e06f7b90b276eeb1ea45714f9b754
Jborro98 <96505624+Jborro98@users.noreply.github.com>
2023-11-13 20:09:20 +0000

    Juego de piedra papel o tijeras con copilot

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+# necesito hacer un juego de rock paper o scissors de un jugador vs ia, el jugador debe colocar una de las 3 
+# respuestas sino el programa volverá a pedir seleccionar una opcion, al jugador se le debe preguntar si desea 
+# jugar otra ronda y en caso de ya no querer se mostrará la puntuacion final
+import random
 
-from flask import Flask
-app = Flask(__name__)
+opciones = ['rock', 'paper', 'scissors']
+puntuacion_jugador = 0
+puntuacion_ia = 0
+jugando = True
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+while jugando:
+    # Paso 1
+    jugador = input("Elige rock, paper o scissors: ").lower()
+    while jugador not in opciones:
+        print("Opción no válida, intente de nuevo")
+        jugador = input("Elige rock, paper o scissors: ").lower()
+    
+    # Paso 2
+    ia = random.choice(opciones)
+    print(f"La IA eligió: {ia}")
+    
+    # Paso 3
+    if jugador == ia:
+        print("Empate!")
+    elif jugador == "rock" and ia == "scissors" or jugador == "paper" and ia == "rock" or jugador == "scissors" and ia == "paper":
+        print("¡Ganaste esta ronda!")
+        puntuacion_jugador += 1
+    else:
+        print("Gana la IA esta ronda")
+        puntuacion_ia += 1
+
+    # Paso 4
+    seguir_jugando = input("¿Quieres jugar otra ronda? (s/n)").lower()
+    while seguir_jugando != 's' and seguir_jugando != 'n':
+        print("Opción no válida, intente de nuevo")
+        seguir_jugando = input("¿Quieres jugar otra ronda? (s/n)").lower()
+
+    # Paso 5
+    if seguir_jugando == 'n':
+        jugando = False
+
+# Paso 6
+print(f"Puntuación final:\nJugador: {puntuacion_jugador}\nIA: {puntuacion_ia}")
